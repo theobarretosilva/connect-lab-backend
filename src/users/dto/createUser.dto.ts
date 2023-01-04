6fcbdff00508b14e520221e664763eecd2bb27d1
Théo Barreto Silva <barretotheo25@gmail.com>
2023-01-04 11:15:37 -0300

    fazendo validações na dto do usuario

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -4,51 +4,63 @@ import {
   IsEmail,
   IsNotEmpty,
   IsOptional,
+  Matches,
 } from 'class-validator';
 import { Match } from 'src/core/constraints/match.decorator';
 
 export class CreateUserDTO {
-  @IsNotEmpty()
-  @IsString()
+  @IsNotEmpty({ message: 'O nome completo é obrigatório!' })
+  @IsString({ message: 'O nome completo deve ser uma string!' })
   readonly fullName: string;
 
-  @IsString()
+  @IsString({ message: 'O link da foto deve ser uma string!' })
   @IsOptional()
-  readonly photoUrl?: string | null =
+  readonly photoUrl?: string =
     'https://github.com/theobarretosilva/Connect-Lab/blob/main/Connect-Lab/src/assets/imgs/userGenerico.png?raw=true';
 
-  @IsString()
-  @IsEmail(undefined, { message: 'O e-mail informado não é válido' })
+  @IsNotEmpty({ message: 'O email é obrigatório!' })
+  @IsString({ message: 'O email deve ser uma string!' })
+  @IsEmail(undefined, { message: 'O e-mail informado não é válido!' })
   readonly email: string;
 
-  @IsString()
-  @MinLength(8)
+  @IsNotEmpty({ message: 'A senha é um campo obrigatório!' })
+  @IsString({ message: 'A senha deve ser uma string!' })
+  @MinLength(8, { message: 'A senha deve ter no mínimo 8 caracteres!' })
   readonly password: string;
 
-  @IsString()
-  @MinLength(8)
-  @Match('password')
+  @IsNotEmpty({ message: 'A confirmação da senha é obrigatória!' })
+  @IsString({ message: 'A confirmação da senha deve ser uma string!' })
+  @MinLength(8, {
+    message: 'A confirmação da senha deve ter no mínimo 8 caracteres!',
+  })
+  @Match('password', { message: 'As senhas não estão iguais, revise-as!' })
   readonly confirmPassword: string;
 
-  @IsString()
+  @IsString({ message: 'O telefone deve ser uma string!' })
   @IsOptional()
   readonly phone?: string | null;
 
+  @IsNotEmpty()
   @IsString()
   readonly zipCode: string;
 
+  @IsNotEmpty()
   @IsString()
   readonly street: string;
 
+  @IsNotEmpty()
   @IsString()
   readonly number: string;
 
+  @IsNotEmpty()
   @IsString()
   readonly neighborhood: string;
 
+  @IsNotEmpty()
   @IsString()
   readonly city: string;
 
+  @IsNotEmpty()
   @IsString()
   readonly state: string;
 
